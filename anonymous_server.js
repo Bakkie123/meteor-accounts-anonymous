aafeebe467988dc95e9b1b5c6156081571ff0487
Alex <alex@hp.(none)>
2013-03-23 02:23:42 +0700

    make compatible with meteor release 0.5.9

--- anonymous_server.js
+++ anonymous_server.js
@@ -7,11 +7,9 @@
     
     // ok; if they are logging in, this means they don't have
     // a user yet. Create one. We don't need to ever find it again.
-    var user = Meteor.accounts.onCreateUserHook({}, {}, {exists: true});
-    var userId = Meteor.users.insert(user);
-    
-    // create a login token (this is how we remember them)
-    var loginToken = Meteor.accounts._loginTokens.insert({userId: userId});
-    return {token: loginToken, id: userId};
+    var user = {services: {}};
+    options = _.clone(options);
+    options.generateLoginToken = true;
+    return Accounts.insertUserDoc(options, user);
   });
 })();
