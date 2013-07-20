bbf578905e5cbb640300c2df7e1e12e7ca1d879b
Alex <alex@hp.(none)>
2013-07-20 17:20:23 +0700

    error handling

--- anonymous_client.js
+++ anonymous_client.js
@@ -1,6 +1,8 @@
 (function() {
   Meteor.loginAnonymously = function(fn) {
     Meteor.call('login', {anonymous: true}, function(err, result) {
+      if (err) 
+        throw err
       Accounts._makeClientLoggedIn(result.id, result.token);
       fn && fn();
     });
