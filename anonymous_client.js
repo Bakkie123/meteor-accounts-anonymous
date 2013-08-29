14e8cee3e9c25371a2b8c6fffa8551309cccd720
Alex <alex@hp.(none)>
2013-08-30 01:48:12 +0700

    make compatible w/ Meteor 0.6.5

--- anonymous_client.js
+++ anonymous_client.js
@@ -1,10 +1,5 @@
 (function() {
   Meteor.loginAnonymously = function(fn) {
-    Meteor.call('login', {anonymous: true}, function(err, result) {
-      if (err) 
-        throw err
-      Accounts.makeClientLoggedIn(result.id, result.token);
-      fn && fn();
-    });
+    Accounts.callLoginMethod({methodArguments:[{anonymous:true}],userCallback:fn});
   }
 })();
